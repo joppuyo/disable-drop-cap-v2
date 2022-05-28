3910b45baf63b5163004276c099a88010ea3af6d
Johannes Siipola <johannes.siipola@knowit.fi>
2022-05-28 20:08:59 +0300

    Release v2.1.4

--- disable-drop-cap.php
+++ disable-drop-cap.php
@@ -8,7 +8,7 @@ if (!defined('ABSPATH')) {
  * Plugin name: Disable Drop Cap
  * Description: Plugin to disable drop cap in Gutenberg editor paragraph block
  * Plugin URI: https://github.com/joppuyo/disable-drop-cap-v2
- * Version: 2.1.3
+ * Version: 2.1.4
  * Requires at least: 5.5
  * Requires PHP: 7.0
  * Author: Johannes Siipola
